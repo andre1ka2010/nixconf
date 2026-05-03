680e4ff2a12d436ca820da81e0336e7f81575dce
andre1ka2010 <141941391+andre1ka2010@users.noreply.github.com>
2026-05-03 19:52:10 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,47 +1,75 @@
+
 { config, pkgs, ... }:
 {
-imports = [
-    ./hardware-configuration.nix
-    ./apps.nix
-    ./services.nix
-    ./automount.nix
-	./firewall.nix
-  ];
+
+imports =
+[
+  ./hardware-configuration.nix
+  ./willnotchange.nix
+  ./testing.nix
+];
 
 boot.loader.systemd-boot.enable = true;
 boot.loader.efi.canTouchEfiVariables = true;
-boot.kernelParams = [ "quiet" "splash" "btusb.enable_autosuspend=n" "amd_pstate=active" "amdgpu.ppfeaturemask=0xffffffff"];  
-boot.plymouth.enable = true;
+boot.kernelParams = [ "loglevel=4" "nowatchdog" "btusb.enable_autosuspend=n" "amd_pstate=active" "amdgpu.ppfeaturemask=0xffffffff"];
 
-networking.hostName = "nixdesktop";
+boot.kernelPackages = pkgs.linuxPackages_latest;
 
-time.timeZone = "Europe/Kyiv";
-i18n.defaultLocale = "en_US.UTF-8";
-i18n.extraLocaleSettings = {
-	LC_ADDRESS = "uk_UA.UTF-8";
-	LC_IDENTIFICATION = "uk_UA.UTF-8";
-	LC_MEASUREMENT = "uk_UA.UTF-8";
-	LC_MONETARY = "uk_UA.UTF-8";
-	LC_NAME = "uk_UA.UTF-8";
-	LC_NUMERIC = "uk_UA.UTF-8";
-	LC_PAPER = "uk_UA.UTF-8";
-	LC_TELEPHONE = "uk_UA.UTF-8";
-	LC_TIME = "uk_UA.UTF-8";
+# Enable sound with pipewire.
+services.pulseaudio.enable = false;
+security.rtkit.enable = true;
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  };
+
+users.users.evoq = {
+  isNormalUser = true;
+  description = "Andrii Haliev";
+  extraGroups = [ "networkmanager" "wheel" "input" "gamemode" "storage"];
+  shell = pkgs.fish;
+  packages = with pkgs; [
+     kdePackages.kate
+  ];
 };
 
-users.users.andre1ka = {
-	isNormalUser = true;
-	description = "Andrii Haliev";
-	extraGroups = [ "networkmanager" "wheel" "gamemode" "storage" "input" "kvm"];
-	shell = pkgs.fish;
-	packages = with pkgs; [
-		kdePackages.kate
-	];
-};
+programs.firefox.enable = true;
+programs.kdeconnect.enable = true;
+programs.gamemode.enable = true;
+programs.fish.enable = true;
+programs.steam.enable = true;
 
+  # $ nix search wget
+environment.systemPackages = with pkgs; [
+  mangohud
+  wget
+  curl
+  git
+  vim
+  zip
+  unzip
+  unrar
+  p7zip
+  cava
+  fastfetch
+  nerd-fonts.jetbrains-mono
+  lact
+  obs-studio
+  easyeffects
+  haruna
+  kitty
+  goverlay
+  prismlauncher
+  vesktop
+  telegram-desktop
+  libreoffice-qt6-fresh
+  javaPackages.compiler.temurin-bin.jdk-25
+];
 
+  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedUDPPorts = [ 25565 ];
+  networking.firewall.enable = true;
 
-boot.tmp.useTmpfs = true;
-
-system.stateVersion = "25.11";
 }
---
+++ willnotchange.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+{
+
+system.stateVersion = "25.11";
+nixpkgs.config.allowUnfree = true;
+networking.networkmanager.enable = true;
+time.timeZone = "Europe/Kyiv";
+i18n.defaultLocale = "en_US.UTF-8";
+i18n.extraLocaleSettings = {
+  LC_ADDRESS = "uk_UA.UTF-8";
+  LC_IDENTIFICATION = "uk_UA.UTF-8";
+  LC_MEASUREMENT = "uk_UA.UTF-8";
+  LC_MONETARY = "uk_UA.UTF-8";
+  LC_NAME = "uk_UA.UTF-8";
+  LC_NUMERIC = "uk_UA.UTF-8";
+  LC_PAPER = "uk_UA.UTF-8";
+  LC_TELEPHONE = "uk_UA.UTF-8";
+  LC_TIME = "uk_UA.UTF-8";
+};
+services.desktopManager.plasma6.enable = true;
+networking.hostName = "nixos";
+services.flatpak.enable = true;
+services.fstrim.enable = true;
+services.lact.enable = true;
+services.displayManager.plasma-login-manager.enable = true;
+services.displayManager.autoLogin = {
+	enable = true;
+	user = "evoq";
+};
+boot.tmp.useTmpfs = true;
+fileSystems."/mnt/sdb" = {
+	device  = "/dev/disk/by-uuid/692bdaaf-cd2d-4fa5-ae0d-9512f07b1833";
+	fsType  = "ext4";
+	options = [ "defaults" "nofail" "x-gvfs-show" "noatime" ];
+};
+hardware.bluetooth = {
+	enable = true;
+	powerOnBoot = true;
+};
+
+}
