4049bd0068dba94a70de5238d39a4af2f5a221d1
andre1ka2010 <141941391+andre1ka2010@users.noreply.github.com>
2026-05-28 17:32:58 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,19 @@
-
 { config, pkgs, ... }:
 {
-
-imports =
-[
+imports = [
   ./hardware-configuration.nix
-  ./willnotchange.nix
-  ./testing.nix
+  ./nochange.nix
+  ./fixfonttest.nix
 ];
 
-boot.loader.systemd-boot.enable = true;
-boot.loader.efi.canTouchEfiVariables = true;
-boot.kernelParams = [ "loglevel=4" "nowatchdog" "btusb.enable_autosuspend=n" "amd_pstate=active" "amdgpu.ppfeaturemask=0xffffffff"];
+boot.kernelParams = [ "loglevel=4" "splash" "quiet" "nowatchdog" "btusb.enable_autosuspend=n" "amd_pstate=active" "amdgpu.ppfeaturemask=0xffffffff" "split_lock_detect=off"];
+boot.kernelPackages = pkgs.linuxPackages_zen;
+boot.plymouth.theme = "bgrt";
+boot.initrd.verbose = false;
 
-boot.kernelPackages = pkgs.linuxPackages_latest;
+networking.hostName = "nixos";
+
+networking.networkmanager.enable = true;
 
 services.pipewire.jack.enable = true;
 services.pulseaudio.enable = false;
@@ -25,13 +25,13 @@ services.pipewire = {
   pulse.enable = true;
 };
 
-users.users.evoq = {
+users.users.andre1ka = {
   isNormalUser = true;
-  description = "Andrii Haliev";
-  extraGroups = [ "networkmanager" "wheel" "input" "gamemode" "storage"];
+  description = "andre1ka";
   shell = pkgs.fish;
+  extraGroups = [ "networkmanager" "wheel" "input" "gamemode" "storage"];
   packages = with pkgs; [
-     kdePackages.kate
+    kdePackages.kate
   ];
 };
 
@@ -40,36 +40,33 @@ programs.kdeconnect.enable = true;
 programs.gamemode.enable = true;
 programs.fish.enable = true;
 programs.steam.enable = true;
-
-environment.systemPackages = with pkgs; [
-  mangohud
-  wget
-  curl
-  git
-  vim
-  zip
-  unzip
-  unrar
-  p7zip
-  cava
-  fastfetch
-  nerd-fonts.jetbrains-mono
-  lact
-  obs-studio
-  easyeffects
-  haruna
-  kitty
-  goverlay
-  prismlauncher
-  vesktop
-  telegram-desktop
-  libreoffice-qt6-fresh
-  javaPackages.compiler.temurin-bin.jdk-25
-];
-
+programs.gamescope.enable = true;
+programs.obs-studio.enable = true;
 networking.firewall.allowedTCPPorts = [ 25565 ];
 networking.firewall.allowedUDPPorts = [ 25565 ];
-networking.firewall.enable = true;
-networking.firewall.allowPing = false; 
+
+environment.systemPackages = with pkgs; [
+mangohud
+mission-center
+wget
+curl
+git
+zip
+unzip
+lact
+unrar
+p7zip
+cava
+fastfetch
+easyeffects
+haruna
+kitty
+prismlauncher
+vesktop
+telegram-desktop
+libreoffice-qt6-fresh
+eza
+bat
+];
 
 }
