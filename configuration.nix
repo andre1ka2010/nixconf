ea902029d8ae6b02af723c0f1e0303f6d49dc501
andre1ka2010 <141941391+andre1ka2010@users.noreply.github.com>
2026-07-10 20:54:14 +0300

    new cfg from scratch in one file
    Refactor NixOS configuration to streamline imports, update kernel parameters, and enable additional services.

--- configuration.nix
+++ configuration.nix
@@ -1,72 +1,154 @@
 { config, pkgs, ... }:
+
 {
-imports = [
-  ./hardware-configuration.nix
-  ./nochange.nix
-  ./fixfonttest.nix
-];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
-boot.kernelParams = [ "loglevel=4" "splash" "quiet" "nowatchdog" "btusb.enable_autosuspend=n" "amd_pstate=active" "amdgpu.ppfeaturemask=0xffffffff" "split_lock_detect=off"];
-boot.kernelPackages = pkgs.linuxPackages_zen;
-boot.plymouth.theme = "bgrt";
-boot.initrd.verbose = false;
-
-networking.hostName = "nixos";
-
-networking.networkmanager.enable = true;
-
-services.pipewire.jack.enable = true;
-services.pulseaudio.enable = false;
-security.rtkit.enable = true;
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-};
-
-users.users.andre1ka = {
-  isNormalUser = true;
-  description = "andre1ka";
-  shell = pkgs.fish;
-  extraGroups = [ "networkmanager" "wheel" "input" "gamemode" "storage"];
-  packages = with pkgs; [
-    kdePackages.kate
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+  #"quiet"
+  #"splash"
+  "amd_pstate=active"
+  "btusb.enable_autosuspend=0"
+  #"amdgpu.ppfeaturemask=0xffffffff"
   ];
-};
 
-programs.firefox.enable = true;
-programs.kdeconnect.enable = true;
-programs.gamemode.enable = true;
-programs.fish.enable = true;
-programs.steam.enable = true;
-programs.gamescope.enable = true;
-programs.obs-studio.enable = true;
-networking.firewall.allowedTCPPorts = [ 25565 ];
-networking.firewall.allowedUDPPorts = [ 25565 ];
 
-environment.systemPackages = with pkgs; [
-mangohud
-mission-center
-wget
-curl
-git
-zip
-unzip
-lact
-unrar
-p7zip
-cava
-fastfetch
-easyeffects
-haruna
-kitty
-prismlauncher
-vesktop
-telegram-desktop
-libreoffice-qt6-fresh
-eza
-bat
-];
+  networking.hostName = "hmmilyall";
+
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Kyiv";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "uk_UA.UTF-8";
+    LC_IDENTIFICATION = "uk_UA.UTF-8";
+    LC_MEASUREMENT = "uk_UA.UTF-8";
+    LC_MONETARY = "uk_UA.UTF-8";
+    LC_NAME = "uk_UA.UTF-8";
+    LC_NUMERIC = "uk_UA.UTF-8";
+    LC_PAPER = "uk_UA.UTF-8";
+    LC_TELEPHONE = "uk_UA.UTF-8";
+    LC_TIME = "uk_UA.UTF-8";
+  };
+
+  services.xserver.enable = false;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.desktopManager.plasma6.enable = true;
+  
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-user-session";
+        user = "greeter";
+      };
+    };
+  };
+
+
+
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    #jack.enable = true;
+  };
+
+  users.users."user" = {
+    isNormalUser = true;
+    description = "User";
+    extraGroups = [ "networkmanager" "wheel" "gamemode" "input" "video" ];
+    packages = with pkgs; [ ];
+    shell = pkgs.fish;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.obs-studio.enable = true;
+  programs.steam.enable = true;
+  programs.gamemode.enable = true;
+  programs.kdeconnect.enable = true;
+  programs.firefox.enable = true;
+  programs.fish.enable = true;
+
+  programs.fish.shellAliases = {
+  nixconf = "sudo nano /etc/nixos/configuration.nix";
+  rebuild = "sudo nixos-rebuild switch";
+  rebuildgrade = "sudo nixos-rebuild switch --upgrade";
+  rebuildboot = "sudo nixos-rebuild boot";
+  rebuildbootgrade = "sudo nixos-rebuild boot --upgrade";
+  };
+
+  environment.systemPackages = with pkgs; [
+
+    # --GUI--
+    kitty
+    kdePackages.kate
+    kdePackages.ksystemlog
+    prismlauncher
+    easyeffects
+    telegram-desktop
+    haruna
+    mission-center
+    goverlay
+    mangohud
+    discord
+
+    # --CLI--
+    zip
+    unzip
+    unrar
+    fastfetch
+    git
+    curl
+    wget
+    p7zip
+
+  ];
+
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      jetbrains-mono
+      terminus_font
+    ];
+  };
+
+  hardware.cpu.amd.updateMicrocode = true;
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  };
+
+  services.fstrim.enable = true;
+  services.lact.enable = true;
+  services.flatpak.enable = true; # spotify
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 25;
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 25565 ];
+  networking.firewall.allowedUDPPorts = [ 25565 ];
+
+  system.stateVersion = "26.05";
 
 }
